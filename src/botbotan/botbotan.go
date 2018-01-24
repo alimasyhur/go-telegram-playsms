a21f5161b8575fd0a3d64f7587eb0601d2d72531
Ali Masyhur <alimasyhur@student.uns.ac.id>
2018-01-24 14:17:49 +0700

    changes message bedanomer message

--- src/botbotan/botbotan.go
+++ src/botbotan/botbotan.go
@@ -42,7 +42,7 @@ func getMessage(incomingMessage []string) (message string) {
 	} else if incomingMessage[0] == CommandSMS {
 		message = strings.Join(incomingMessage[2:], " ")
 	} else if incomingMessage[0] == CommandBedanomer {
-		message = "mohon maaf. permintaan token harus dari nomer pengirim yang sama dengan nomer yang terdaftar di siakad atau simpeg."
+		message = "mohon maaf. permintaan token hrs dr no pengirim yg sama dg yg terdaftar di siakad/simpeg. Update nomer di siakad.uns.ac.id/registrasi atau ke operator simpeg"
 	}
 	return
 }
